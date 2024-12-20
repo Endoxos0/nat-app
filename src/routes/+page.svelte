f9bd0a9350c3609c388020af44220cadd94b77eb
Endoxos0 <97339649+Endoxos0@users.noreply.github.com>
2024-12-20 21:01:15 +0100

    video

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to reasdddddddddddddad the documentation</p>
+<iframe
+    width="1519"
+    height="607"
+    src="https://www.youtube.com/embed/j1_MrnDEL1M"
+    title="voorstel natuurkunde"
+    frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+    referrerpolicy="strict-origin-when-cross-origin"
+    allowfullscreen
+></iframe>
